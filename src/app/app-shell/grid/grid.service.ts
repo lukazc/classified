2860584c20ca24f3f9529c1044beda6972b4a4a2
lukazc <luka.zarkovic.cerneli@gmail.com>
2019-09-27 23:23:07 +0200

    Add GET POST & DELETE image to grid service.

--- src/app/app-shell/grid/grid.service.ts
+++ src/app/app-shell/grid/grid.service.ts
@@ -5,22 +5,34 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class GridService {
-
-  private listUrl = environment.endpoint.grid;
-
-  constructor(private http: HttpClient) { }
-
-  getGridItems(): Observable<IGridTextItem[]> {
-    return this.http.get<IGridTextItem[]>(this.listUrl);
-  }
+	
+	private imagesUrl = environment.endpoint.image;
+	
+	constructor(private http: HttpClient) { }
+	
+	getImages(): Observable<IGridImageItem[]> {
+		return this.http.get<IGridImageItem[]>(this.imagesUrl);
+	}
+	
+	uploadImage(image): Observable<IGridImageItem> {
+		
+		const formData =  new FormData();
+		formData.append('image', image);
+		
+		return this.http.post<IGridImageItem>(this.imagesUrl, formData);
+	}
+	
+	deleteImage(name: string) {
+		return this.http.delete(`${environment.endpoint.image}/${name}`);
+	}
+	
 }
 
-
-export interface IGridTextItem {
-  description: string;
-  header: string;
-  id: number;
+export interface IGridImageItem {
+	b64: string;
+	filename: string;
+	labelAnnotations: Object;
 }
