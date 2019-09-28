e2ca23d6f911c55aee331698a1012f2a748e8f5f
lukazc <luka.zarkovic.cerneli@gmail.com>
2019-09-28 17:05:25 +0200

    Delete annotations on image deletion

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,6 +20,7 @@ const client = new vision.ImageAnnotatorClient({
 });
 
 const router = express.Router();
+
 // MasterDetail Page Endpoint
 router.get(CONSTANTS.ENDPOINT.MASTERDETAIL, (req, res) => {
 	res.json(sampleData.textAssets);
@@ -32,8 +33,8 @@ router.get(CONSTANTS.ENDPOINT.LIST, function(req, res) {
 
 router.post(CONSTANTS.ENDPOINT.LIST, function(req, res) {
 	let listItem = {
-		text: req.body.text,
-		_id: sampleData.listID
+	text: req.body.text,
+	_id: sampleData.listID
 	};
 	sampleData.listTextAssets.unshift(listItem);
 	res.json(listItem);
@@ -43,156 +44,181 @@ router.post(CONSTANTS.ENDPOINT.LIST, function(req, res) {
 router.delete(CONSTANTS.ENDPOINT.LIST + "/:_id", function(req, res) {
 	const { _id } = req.params;
 	var index = sampleData.listTextAssets.findIndex(
-		listItem => listItem._id === Number(_id)
-		);
-		if (index > -1) {
-			sampleData.listTextAssets.splice(index, 1);
-			res.json({ _id: Number(_id), text: "This commented was deleted" });
-		} else {
-			res.status(404).send("Could not find item with id:" + _id);
-		}
-	});
-	
-	// IMAGE ENDPOINTS
-	// upload image
-	router.post(CONSTANTS.ENDPOINT.IMAGE, upload.single('image'), function(req, res) {
-		console.log(req.file);
+	listItem => listItem._id === Number(_id)
+	);
+	if (index > -1) {
+		sampleData.listTextAssets.splice(index, 1);
+		res.json({ _id: Number(_id), text: "This commented was deleted" });
+	} else {
+		res.status(404).send("Could not find item with id:" + _id);
+	}
+});
+
+// IMAGE ENDPOINTS
+// upload image
+router.post(CONSTANTS.ENDPOINT.IMAGE, upload.single('image'), function(req, res) {
+	console.log(req.file);
+
+	const filename = req.file.filename;
+	const filePath = req.file.path;
+
+	// read binary data
+	const bitmap = fs.readFileSync(filePath);
+	// convert binary data to base64 encoded string
+	const b64 = new Buffer.from(bitmap).toString('base64');
+
+	// pass response as callback to wait for Vision API response
+	annotateImage({ filename, filePath, b64 }, res);
+
+	// res.json(imageAnnotations);
+});
+
+// get images
+router.get(CONSTANTS.ENDPOINT.IMAGE, function(req, res) {
+
+	let base64Images = [];
+
+	readDatastore();
+	datastoreImages.forEach(filename => {
+		// fs.createReadStream(path.join(datastorePath, filename), { encoding: 'base64' }).pipe(res);
 		
-		const filename = req.file.filename;
-		const filePath = req.file.path;
+		let image = {};
+		image['filename'] = filename;
 		
 		// read binary data
-		const bitmap = fs.readFileSync(filePath);
+		let bitmap = fs.readFileSync(path.join(datastorePath, filename));
 		// convert binary data to base64 encoded string
-		const b64 = new Buffer.from(bitmap).toString('base64');
-
-		// pass response as callback to wait for Vision API response
-		annotateImage({ filename, filePath, b64 }, res);
-
-		// res.json(imageAnnotations);
-	});
-	
-	// get images
-	router.get(CONSTANTS.ENDPOINT.IMAGE, function(req, res) {
-
-		let base64Images = [];
-		
-		readDatastore();
-		datastoreImages.forEach(filename => {
-			// fs.createReadStream(path.join(datastorePath, filename), { encoding: 'base64' }).pipe(res);
-			
-			let image = {};
-			image['filename'] = filename;
-
-			// read binary data
-			let bitmap = fs.readFileSync(path.join(datastorePath, filename));
-			// convert binary data to base64 encoded string
-			let b64 = new Buffer.from(bitmap).toString('base64');
-			// add image to response
-			image['b64'] = 'data:image/jpeg;base64,' + b64;
-			// add label annotations from Vision API
-			let imageAnnotations;
-			try {
-				imageAnnotations = JSON.parse(fs.readFileSync('imageAnnotations.json', { encoding: 'utf8' }));
-			} catch (error) {
-				imageAnnotations = {};
-				console.log('GET IMAGES readFile imageAnnotations ERROR -> ', error);
-			}
-			image['labelAnnotations'] = imageAnnotations[filename] ? imageAnnotations[filename]['labelAnnotations'] : null;
-			
-			base64Images.push(image);
-		});
-		
-		res.json(base64Images);
-	});
-	
-	router.delete(CONSTANTS.ENDPOINT.IMAGE + "/:name", function(req, res) {
-		const { name } = req.params;
-		fs.unlink(path.join(datastorePath, name), function(err) {
-			if(err) throw err;
-
-			// TODO delete annotations from JSON
-			
-			console.log('File deleted ' + name);
-			res.json({ name: name, text: "This image was deleted" });
-		});
-	});
-	
-	
-	// Grid Page Endpoint
-	router.get(CONSTANTS.ENDPOINT.GRID, (req, res) => {
-		res.json(sampleData.textAssets);
-	});
-	
-	function readDatastore() {
-		
-		datastoreImages = fs.readdirSync(datastorePath);
-		// //passsing directoryPath and callback function
-		// fs.readdir(datastorePath, function (err, files) {
-		//   //handling error
-		//   if (err) {
-		//       return console.log('Unable to scan directory: ' + err);
-		//   } 
-		
-		//   datastoreImages = [];
-		//   //listing all files using forEach
-		//   files.forEach(function (file) {
-		//       // Do whatever you want to do with the file
-		//       datastoreImages.push(file); 
-		//   });
-		// });
-	}
-
-	async function annotateImage({ filename, filePath, b64 }, response = false) {
+		let b64 = new Buffer.from(bitmap).toString('base64');
+		// add image to response
+		image['b64'] = 'data:image/jpeg;base64,' + b64;
+		// add label annotations from Vision API
+		let imageAnnotations;
 		try {
-			// TODO test additional features
-			const [result] = await client.annotateImage(
-				{
-					"image":{
-					  "content": b64,
-					},
-					"features": [
-					  {
+			imageAnnotations = JSON.parse(fs.readFileSync('imageAnnotations.json', { encoding: 'utf8' }));
+		} catch (error) {
+			imageAnnotations = {};
+			console.log('GET IMAGES readFile imageAnnotations ERROR -> ', error);
+		}
+		image['labelAnnotations'] = imageAnnotations[filename] ? imageAnnotations[filename]['labelAnnotations'] : null;
+		
+		base64Images.push(image);
+	});
+
+	res.json(base64Images);
+});
+
+router.delete(CONSTANTS.ENDPOINT.IMAGE + "/:filename", function(req, res) {
+	const { filename } = req.params;
+	fs.unlink(path.join(datastorePath, filename), function(err) {
+		if(err) throw err;
+		
+		// TODO delete annotations from JSON
+		deleteImageAnnotations(filename);
+		
+		console.log('File deleted ' + filename);
+		res.json({ filename: filename, text: "This image was deleted" });
+	});
+});
+
+
+// Grid Page Endpoint
+router.get(CONSTANTS.ENDPOINT.GRID, (req, res) => {
+	res.json(sampleData.textAssets);
+});
+
+function readDatastore() {
+
+	datastoreImages = fs.readdirSync(datastorePath);
+	// //passsing directoryPath and callback function
+	// fs.readdir(datastorePath, function (err, files) {
+	//   //handling error
+	//   if (err) {
+	//       return console.log('Unable to scan directory: ' + err);
+	//   } 
+
+	//   datastoreImages = [];
+	//   //listing all files using forEach
+	//   files.forEach(function (file) {
+	//       // Do whatever you want to do with the file
+	//       datastoreImages.push(file); 
+	//   });
+	// });
+}
+
+async function annotateImage({ filename, filePath, b64 }, response = false) {
+	try {
+		// TODO test additional features
+		const [result] = await client.annotateImage(
+			{
+				"image":{
+					"content": b64,
+				},
+				"features": [
+					{
 						"type": "LABEL_DETECTION",
 						"maxResults": 5
-					  }
-					]
-				  }
-			);
-			
-			fs.readFile('imageAnnotations.json', 'utf8', (err, data) => {
-
-				let imageAnnotations;
-
-				if (err){ 
-					imageAnnotations = {};
-					console.log('readFile imageAnnotations ERROR -> ', err);
-				} else {
-					imageAnnotations = JSON.parse(data);
-				}
-
-				//add annotations
-				imageAnnotations[filename] = result;
-				//convert it back to json
-				json = JSON.stringify(imageAnnotations);
-				// write to disk 
-				fs.writeFile('imageAnnotations.json', json, 'utf8', (error) => {
-					console.log('writeFile imageAnnotations ERROR -> ', error)
-				});
-			});
-
-			// append encoding type to beginning so the img src can interpret it
-			b64 = 'data:image/jpeg;base64,' + b64;
+					}
+				]
+			}
+		);
 		
-			let labelAnnotations = result['labelAnnotations'];
-
-			if (response) response.json({ filename, b64, labelAnnotations });
-
-		} catch (error) {
-			console.log('annotateImage ERROR ', error);
-		}
+		fs.readFile('imageAnnotations.json', 'utf8', (err, data) => {
+			
+			let imageAnnotations;
+			
+			if (err){ 
+				imageAnnotations = {};
+				console.log('readFile imageAnnotations ERROR -> ', err);
+			} else {
+				imageAnnotations = JSON.parse(data);
+			}
+			
+			//add annotations
+			imageAnnotations[filename] = result;
+			//convert it back to json
+			json = JSON.stringify(imageAnnotations);
+			// write to disk 
+			fs.writeFile('imageAnnotations.json', json, 'utf8', (error) => {
+				if(error) console.log('writeFile imageAnnotations ERROR -> ', error)
+			});
+		});
+		
+		// append encoding type to beginning so the img src can interpret it
+		b64 = 'data:image/jpeg;base64,' + b64;
+		
+		let labelAnnotations = result['labelAnnotations'];
+		
+		if (response) response.json({ filename, b64, labelAnnotations });
+		
+	} catch (error) {
+		console.log('annotateImage ERROR ', error);
 	}
-	
-	
-	module.exports = router;
-	
+}
+
+function deleteImageAnnotations(filename) {
+	fs.readFile('imageAnnotations.json', 'utf8', (err, data) => {
+		
+		let imageAnnotations;
+		
+		if (err){ 
+			console.log('deleteImageAnnotations() -> readFile imageAnnotations ERROR -> ', err);
+			return;
+		} else {
+			imageAnnotations = JSON.parse(data);
+		}
+		
+		// delete annotations
+		delete imageAnnotations[filename];
+		
+		console.log('deleteImageAnnotations() -> annotations after delete ', imageAnnotations);
+		//convert it back to json
+		json = JSON.stringify(imageAnnotations);
+		// write to disk 
+		fs.writeFile('imageAnnotations.json', json, 'utf8', (error) => {
+			if(error) console.log('writeFile imageAnnotations ERROR -> ', error)
+		});
+	});
+}
+
+
+module.exports = router;
